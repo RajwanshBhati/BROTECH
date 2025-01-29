fa4a67c09234d4c245a69de1a3975778bca71f6a
Rajwansh Bhati <rajwanshbhati@gmail.com>
2025-01-29 22:52:16 +0530

    first commit

---
+++ vite-project/src/components/services.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Our Services</h2>
+          <p>
+          We offer innovative web and mobile app development, full-stack solutions, UI/UX design, e-commerce, cloud integration, AI automation, custom software development, and ongoing maintenance to drive your business forward
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
